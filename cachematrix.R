8e607f8cb5834722c25ecc9ffc1b9bbb966e911d
Elena Shchur <shchure@gmail.com>
2015-02-22 23:00:25 +0000

    PA2 completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Inversing a matrix is a time-consuming operation especially for large matrices. In case we need to inverse the same matrix several times, it could make sense to cache its inverse. We introduce two functions which allow to cache matrix'x inverse instead of recomputing it.
+   
+## The first function, makeCacheMatrix creates a special object, which includes a matrix and the ability to store its inverse. Actually it's a list of 4 functions that	
+## 1.	set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse
+## 4. get the value of the inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+ makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## The next function calculates the inverse of the "matrix" created with the above function. But at first it checks whether the inverse has already been calculated. If yes, it gets the inverse from the cache and skips the computation. If no, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+##Example:
+#> x <- matrix(1:4, 2, 2)
+#> mat <- makeCacheMatrix()
+#> mat$set(x)
+#> mat$get()
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> cachesolve(mat)
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> cachesolve(mat)
+#getting cached data
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5 
